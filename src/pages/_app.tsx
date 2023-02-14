a2c556ffaef423c7d108c07f6fba2120e212edcc
unknown <bibashrajthala10@gmail.com>
2023-02-14 10:38:37 +0545

    feat: add multipage layout feature

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { NextPageWithLayout } from './page';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available, other wise just return the page itself
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
